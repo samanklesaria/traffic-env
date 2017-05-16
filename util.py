3924b3651764f399fc7cfb777739296c71c6cd66
Sam Anklesaria <anklesaria@aya.yale.edu>
2017-05-16 16:31:35 -0400

    light times not global

--- util.py
+++ util.py
@@ -9,10 +9,9 @@ def square(x): return x*x
 def forever(f):
   while True: yield f()
 
-light_times = []
-
 def print_running_stats(iterator):
   trip_times = []
+  light_times = []
   try:
     reward_mean = 0
     reward_var = 0
@@ -26,6 +25,7 @@ def print_running_stats(iterator):
       if info:
         print("One prob: %2f,\t Zero prob: %2f" % (info['onep'], info['zerop']))
         trip_times.extend(info['trip_times'])
+        light_times.extend(info['light_times'])
   except KeyboardInterrupt:
     print("Interrupted")
     return (light_times, trip_times)
@@ -51,12 +51,12 @@ def write_data(light_times, trip_times):
   np.save("light_times.npy", light_times)
   np.save("trip_times.npy", trip_times)
 
-# TODO- light_times shouldn't be global. Throws off notebook analysis
 def episode_reward(gen):
   num_0s = 0
   num_1s = 0
   reward = 0.0
   multiplier = 1.0
+  light_times = []
   for (i,_,_,r,info,*_) in gen:
     reward += np.mean(r) * (multiplier if FLAGS.print_discounted else 1)
     multiplier *= FLAGS.gamma
@@ -73,7 +73,7 @@ def episode_reward(gen):
     denom = (math.pow(FLAGS.gamma, i+1) - 1) / (FLAGS.gamma - 1) 
   if FLAGS.mode == 'validate':
     total_actions = num_1s + num_0s
-    info_struct = {'zerop': num_0s / total_actions,
+    info_struct = {'zerop': num_0s / total_actions, 'light_times': light_times,
       'onep': num_1s / total_actions, 'trip_times': info['trip_times']}
   else: info_struct = None
   return (reward / denom, info_struct)
